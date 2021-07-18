bea9f57e227af4969651114870f6ad1d1f42679f
Jose Enmanuel Estrella <enmanuelgit@gmail.com>
2021-07-18 17:59:01 -0400

    Add player JumAndFire

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BulletScript : MonoBehaviour
+{
+    public float Speed;
+    public AudioClip Sound;
+
+    private Rigidbody2D Rigidbody2D;
+    private Vector3 Direction;
+    private int kill = 6;
+
+
+    private void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+        Camera.main.GetComponent<AudioSource>().PlayOneShot(Sound);
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody2D.velocity = Direction * Speed;
+    }
+
+    public void SetDirection(Vector3 direction)
+    {
+        Direction = direction;
+    }
+
+    public void DestroyBullet()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        GruntScript grunt = other.GetComponent<GruntScript>();
+        JohnMovement john = other.GetComponent<JohnMovement>();
+        if (grunt != null)
+        {
+            grunt.Hit();
+
+            if (kill == 0)
+            {
+                grunt.KillMethod();
+            }
+        }
+        if (john != null)
+        {
+            john.Hit();
+        }
+        DestroyBullet();
+    }
+}
